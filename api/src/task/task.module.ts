64ef57e9a69a658a306e109657edd717ce1e1fd0
Oliwier Michalik <olimioli9@gmail.com>
2022-07-08 23:25:33 +0200

    add task list

--- api/src/task/task.module.ts
+++ api/src/task/task.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TaskController } from './task/task.controller';
 import { TaskQueueService } from './task-queue/task-queue.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [TaskController],
-  providers: [TaskQueueService],
+  providers: [TaskQueueService, PrismaService],
 })
 export class TaskModule {}
--- frontend/pages/problems.tsx
+++ frontend/pages/problems.tsx
@@ -1,17 +1,44 @@
+import { showNotification } from "@mantine/notifications";
+import axios from "axios";
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import { useEffect, useState } from "react";
+import constants from "../app/constants";
+import { TaskList } from "../components/tasks/TaskList";
 
 const Home: NextPage = () => {
+  const [tasks, setTasks] = useState([]);
+  const loadTasks = async () => {
+    const r = await axios.get(constants.SERVER_URL + "/task").catch(() =>
+      showNotification({
+        color: "red",
+        title: "Error",
+        message: "Could not load tasks, please try again later",
+      })
+    );
+
+    if (r) {
+      setTasks(
+        r.data.map((e: any) => ({
+          id: e.id.toString(),
+          name: e.name,
+          stars: e.stars.toString(),
+          difficulty: e.difficulty.toString(),
+          completions: e.completions.toString(),
+        }))
+      );
+    }
+  };
+  useEffect(() => {
+    loadTasks();
+  }, []);
+
   return (
     <>
       <Head>
         <title>xAlgs | Problems</title>
       </Head>
-      <div>
-        <h1>Problems go here</h1>
-      </div>
+      <TaskList data={tasks} />
     </>
   );
 };
